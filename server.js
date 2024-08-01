5c558921647e0033cc8bbe31aafafa52478e00a1
Megaapis <companhiadasprimas@gmail.com>
2024-08-01 11:21:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-console.log('Iniciando....')
-var express = require('express')
+console.log('Iniciando....');
+var express = require('express');
 var morgan = require('morgan');
-var secure = require('ssl-express-www')
+var secure = require('ssl-express-www');
 
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 10000;
 
-var mainrouter = require('./apis')
+var mainrouter = require('./index.js'); // Importa o roteador principal
 
-var app = express()
+var app = express();
 app.enable('trust proxy');
 app.use(morgan('dev'));
-app.set("json spaces",2)
-app.use(secure)
-app.use(express.static("public"))
+app.set("json spaces", 2);
+app.use(secure);
+app.use(express.static("public"));
 
-app.use('/', mainrouter)
+app.use('/', mainrouter); // Usa o roteador principal
 
 app.listen(PORT, () => {
-    console.log('Conectando...')
-    console.log("Servidor rodando em http://localhost:" + PORT)
-})
+    console.log('Conectando...');
+    console.log("Servidor rodando em http://localhost:" + PORT);
+});
 
 module.exports = app
