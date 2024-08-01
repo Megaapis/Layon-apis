d99ddff7586f68d624c8d03d08c5baffb53df281
megaapis <companhiadasprimas@gmail.com>
2024-07-31 23:48:07 -0300

    fe em deus

---
+++ index2.js
@@ -0,0 +1,24 @@
+console.log('Iniciando....')
+var express = require('express')
+var morgan = require('morgan');
+var secure = require('ssl-express-www')
+
+const PORT = process.env.PORT || 8080
+
+var mainrouter = require('./apis')
+
+var app = express()
+app.enable('trust proxy');
+app.use(morgan('dev'));
+app.set("json spaces",2)
+app.use(secure)
+app.use(express.static("public"))
+
+app.use('/', mainrouter)
+
+app.listen(PORT, () => {
+    console.log('Conectando...')
+    console.log("Servidor rodando em http://localhost:" + PORT)
+})
+
+module.exports = app
